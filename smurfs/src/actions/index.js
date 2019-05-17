0a80a2f3088f0b8503bbfd228f57f93b3364172d
cashcoder88 <cashglobe@gmail.com>
2019-05-17 09:32:25 -0600

    Created action types

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,16 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
+export const FETCHING_SMURFS_FAILURE = 'FETCHING_SMURFS_FAILURE';
+
+export const ADDING_SMURFS = 'ADDING_SMURFS';
+export const ADDING_SMURFS_SUCCESS = 'ADDING_SMURFS_SUCCESS';
+export const ADDING_SMURFS_FAILURE = 'ADDING_SMURFS_FAILURE';
+
+
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
